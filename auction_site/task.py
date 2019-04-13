6c7b3c2f156bfbaf083a0f7aa30d93e452bb4e3b
Akhil Dhavala <akhil@cogoport.com>
2019-04-13 10:15:38 +0530

    updating code and adding emails

--- auction_site/task.py
+++ auction_site/task.py
@@ -1,16 +1,15 @@
-from datetime import datetime
 from items.models import Item
 from bids.models import Bid
 from users.models import User
 import smtplib
 import auction_site.config as config
+from datetime import datetime, timedelta
 
 class WinnerTask:
     def run(self):
-        hour = datetime.now().hour
-        start = datetime.now().replace(microsecond=0,second=0,minute=0, hour=hour-1)
-        end = datetime.now().replace(microsecond=0,second=0,minute=0)
-        completed_items = list(Item.objects.filter(end_date__lte=end, end_date__gt=start).values('id'))
+        start = (datetime.now() - timedelta(hours = 1)).replace(microsecond=0,second=0,minute=0)
+        end = datetime.now().replace(microsecond=0,second=0,minute=0, hour=datetime.now().hour+1)
+        completed_items = list(Item.objects.filter(end_time__lte=end, end_time__gt=start).values('id'))
         completed_item_ids = map(lambda x: x['id'], completed_items)
         bids = list(Bid.objects.filter(item_id__in=completed_item_ids).order_by('amount').values('amount', 'item_id', 'user_id', 'id'))
         item_vals = {}
@@ -38,12 +37,11 @@ class WinnerTask:
                 msg += item_name + "\t" + amount + "\t" + winner_name + "\n"
             msg += "\nRegards"
             msg ='Subject: {}\n\n{}'.format(subject, msg)
-            server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, msg)
+            emails = list(User.objects.values('email'))
+            tos = list(map(lambda x:x['email'], emails))
+            server.sendmail(config.EMAIL_ADDRESS, tos, msg)
             server.quit()
             print("Success: Email sent!")
         except:
             print("Email failed to send.")
 
-
-w = WinnerTask()
-w.run()
