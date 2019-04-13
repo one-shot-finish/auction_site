c435144e67f4bb6f96be30c3260c3a53c42962c5
Akhil Dhavala <akhil.dhavala@gmail.com>
2019-04-13 12:20:59 +0000

    temp save

--- auction_site/settings.py
+++ auction_site/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'j#qi+rc648+9##(qjuc^93m$t*(tee%-pw_y7i27j6a952fz#d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [u'imakhil.pythonanywhere.com']
 
 # Application definition
 
